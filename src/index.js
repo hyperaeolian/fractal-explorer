33ad19493beef14aef3e508b61754fcb164dabed
Rob <milrober@gmail.com>
2017-05-10 22:13:23 -0400

    Convert to instance mode of p5js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,67 @@
 import Complex from './complex';
 
-const MAX_ITERATIONS = 100;
-const INFINITY = 20;
+
+const MandelbrotSet = function(p5){
+    
+    const MAX_ITERATIONS = 700;
+    const INFINITY = 50;
+    const WIDTH = 600;
+    const HEIGHT = 600;
 
 
-window.setup = function() {
-    createCanvas(200, 200);
-    loadPixels();
-    for (let i = 0; i < width; i++) {
-        for (let j = 0; j < height; j++) {
+    p5.setup = function() {
 
-        let Z = Complex.of(
-            map(i, 0, width, -2.5, 2.5),
-            map(j, 0, height, -2.5, 2.5)
-        );
+        p5.createCanvas(WIDTH, HEIGHT);
+        p5.loadPixels();
 
-        let C = Complex.of(Z);
+        for (let i = 0; i < WIDTH; i++) {
+            for (let j = 0; j < HEIGHT; j++) {
 
-        let num_iters = 0;
+            let Z = Complex.of(
+                p5.map(i, 0, WIDTH, -2.5, 2.5),
+                p5.map(j, 0, HEIGHT, -2.5, 2.5)
+            );
 
-        while (Z.magnitude() < INFINITY && num_iters < MAX_ITERATIONS) {
-            // Mandelbrot's equation: Zn+1 = Zn^2 + C
-            Z = Z.multiply(Z).add(C);
-            num_iters++;
+            let C = Complex.of(Z);
+
+            let num_iters = 0;
+
+            while (Z.magnitude() < INFINITY && num_iters < MAX_ITERATIONS) {
+                // Mandelbrot's equation: Zn+1 = Zn^2 + C
+                Z = Z.multiply(Z).add(C);
+                num_iters++;
+            }
+
+            let colorValue = normalizeToRGBValue(num_iters, MAX_ITERATIONS);
+
+            // Uncomment for escape time coloring
+            //if (num_iters === MAX_ITERATIONS) {
+            // bright = 0;
+            //}
+
+            let pix = (i + j * WIDTH) * 4;
+            p5.pixels[pix + 0] = colorValue;
+            p5.pixels[pix + 1] = colorValue;
+            p5.pixels[pix + 2] = colorValue;
+            p5.pixels[pix + 3] = 255;
+            }
         }
-
-        let colorValue = normalizeToRGBValue(num_iters, MAX_ITERATIONS);
-
-        // Uncomment for escape time coloring
-        //if (num_iters === MAX_ITERATIONS) {
-        // bright = 0;
-        //}
-
-        let pix = (i + j * width) * 4;
-        pixels[pix + 0] = colorValue;
-        pixels[pix + 1] = colorValue;
-        pixels[pix + 2] = colorValue;
-        pixels[pix + 3] = 255;
-        }
+        p5.updatePixels();
     }
-    updatePixels();
+
+    p5.draw = function() {
+
+    }
+
+
+    const normalizeToRGBValue = (val, maxValue) => {
+      /*
+       * Take a value from 0 to @param maxValue
+       *   and map it to a scale of 0 to 255
+       */
+        let value = p5.map(val, 0, maxValue, 0, 1);
+        return p5.map(p5.sqrt(value), 0, 1, 0, 255);
+    }
 }
 
-window.draw = function() {
-
-}
-
-
-const normalizeToRGBValue = (val, maxValue) => {
-  /*
-   * Take a value from 0 to @param maxValue
-   *   and map it to a scale of 0 to 255
-   */
-    let value = map(val, 0, maxValue, 0, 1);
-    return map(sqrt(value), 0, 1, 0, 255);
-}
+const FractalApp = new window.p5(MandelbrotSet);
