76ce77ddbc20fd44697b5f27e5f253b767c2fdcf
Rob <milrober@gmail.com>
2017-05-17 23:02:44 -0400

    Convert indentations to spaces

--- src/components/slider.js
+++ src/components/slider.js
@@ -4,49 +4,49 @@ import {div, label, strong, input} from '@cycle/dom'
 
 function Slider(sources){
 
-	// Get the updated value for the slider
-	const update$ = sources.DOM
-		.select('.slider')
-		.events('input')
-		.map(e => e.target.value);
+    // Get the updated value for the slider
+    const update$ = sources.DOM
+        .select('.slider')
+        .events('input')
+        .map(e => e.target.value);
 
-	// Pass props received from parent into the slider's attrs
-	const state$ = sources.props
-		.map(props => update$
-			.map(updatedValue => ({
-				label: props.label,
-				min: props.min,
-				max: props.max,
-				step: props.step,
-				value: updatedValue
-			})).startWith(props)
-		).flatten().remember();
+    // Pass props received from parent into the slider's attrs
+    const state$ = sources.props
+        .map(props => update$
+            .map(updatedValue => ({
+                label: props.label,
+                min: props.min,
+                max: props.max,
+                step: props.step,
+                value: updatedValue
+            })).startWith(props)
+        ).flatten().remember();
 
-	// Construct the template for our labeled slider component
-	const vTree$ = state$.map(state =>
-		div('.param-slider-component', [
-			label('.slider-label', [
-				`${state.label}: `, strong(state.value)
-			]),
-			input('.slider', {
-				attrs: {
-					type: 'range',
-					min: state.min,
-					max: state.max,
-					value: state.value,
-					step: state.step
-				}
-			})
-		])
-	);
+    // Construct the template for our labeled slider component
+    const vTree$ = state$.map(state =>
+        div('.param-slider-component', [
+            label('.slider-label', [
+                `${state.label}: `, strong(state.value)
+            ]),
+            input('.slider', {
+                attrs: {
+                    type: 'range',
+                    min: state.min,
+                    max: state.max,
+                    value: state.value,
+                    step: state.step
+                }
+            })
+        ])
+    );
 
-	// Pass the template and updated value back to the parent
-	const sinks = {
-		DOM: vTree$,
-		value: state$.map(state => state.value)
-	};
+    // Pass the template and updated value back to the parent
+    const sinks = {
+        DOM: vTree$,
+        value: state$.map(state => state.value)
+    };
 
-	return sinks;
+    return sinks;
 }
 
 // Isolate slider instances so that they act independently of one another
--- src/drivers/sketchDriver.js
+++ src/drivers/sketchDriver.js
@@ -2,15 +2,15 @@ import xs from 'xstream'
 
 
 export default function makeSketchDriver(App){
-	
-	function sketchDriver(parameters$){
-		parameters$.addListener({
-			next: params => App.update(params),
-			error: () => { console.warn("An error occurred in sketch Driver"); },
-			complete: () => {}
-		});
-	}
+    
+    function sketchDriver(parameters$){
+        parameters$.addListener({
+            next: params => App.update(params),
+            error: () => { console.warn("An error occurred in sketch Driver"); },
+            complete: () => {}
+        });
+    }
 
-	return sketchDriver;
+    return sketchDriver;
 
 }
