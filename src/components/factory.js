40f0d1445f0dd5e58f821a09c1b53c4e1caf3c21
Rob <milrober@gmail.com>
2017-05-29 18:58:16 -0400

    Add ids to components and convert indentation to spaces

--- src/components/factory.js
+++ src/components/factory.js
@@ -5,36 +5,40 @@ import ToggleButton from './toggleButton'
 
 
 export function Register(components){
-	return {
-		views: components.map(cmpnt => cmpnt.view$),
-		states: components.map(cmpnt => cmpnt.state$)
-	}
+    return {
+        views: components.map(cmpnt => cmpnt.view$),
+        states: components.map(cmpnt => cmpnt.state$)
+    }
 }
 
 export function ComponentFactory(sources){
-	return type => {
-		return props => {
-			let componentType;
-			let params = { DOM: sources.DOM, props };
-			
-			switch(type){
-				case 'Slider':
-					params['props'] = xs.of(props);
-					componentType = Slider;
-					break;
-				case 'Button':
-					componentType = ToggleButton;
-					break;
-			}
 
-			const component = isolate(componentType)(params);
-			
-			return {
-				instance: component,
-				view$: component.DOM,
-				state$: component.value
-			}
-		}
-		
-	}
+    let id = 0;
+
+    return type => {
+        return props => {
+            let componentType;
+            let params = { DOM: sources.DOM, props };
+
+            switch(type){
+                case 'Slider':
+                    params['props'] = xs.of(props);
+                    componentType = Slider;
+                    break;
+                case 'ToggleButton':
+                    componentType = ToggleButton;
+                    break;
+            }
+
+            const component = isolate(componentType)(params);
+
+            return {
+                id: id++,
+                instance: component,
+                view$: component.DOM,
+                state$: component.value
+            }
+        }
+
+    }
 }
