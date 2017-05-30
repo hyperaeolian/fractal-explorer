4f20f93acb8ab84f27b322feb8d42375b12d27d4
Rob <milrober@gmail.com>
2017-05-29 22:05:48 -0400

    Use function-scoped object to manage state

--- src/fractals/mandelbrot.js
+++ src/fractals/mandelbrot.js
@@ -16,18 +16,28 @@ export default new window.p5(function(p){
     let Red = 0;
     let Green = 0;
     let Blue = 0;
-    let Alpha = 0;
+    let Alpha = 250;
+
+    const CurrentState = {
+        maxIterations: 100,
+        upperBound: 20,
+        escapeColoring: false,
+        red: 0,
+        green: 0,
+        blue: 0,
+        alpha: 255
+    };
 
 
     p.update = function(state) {
         console.log(`State: ${JSON.stringify(state)}`);
-        MaxIterations = state.iterations|0;
-        UpperBound = state.bound|0;
-        Red = state.red|0;
-        Green = state.green|0;
-        Blue = state.blue|0;
-        Alpha = state.alpha|0;
-        ColorEscapedPixels = state.esc;
+        CurrentState.maxIterations = state.iterations|0;
+        CurrentState.upperBound = state.bound|0;
+        CurrentState.red = state.red|0;
+        CurrentState.green = state.green|0;
+        CurrentState.blue = state.blue|0;
+        CurrentState.alpha = state.alpha|0;
+        CurrentState.escapeColoring = state.esc;
     }
 
 
@@ -41,23 +51,15 @@ export default new window.p5(function(p){
 
 
     p.draw = function(){
-         _renderMandelbrotSet(
-            MaxIterations,
-            ColorEscapedPixels,
-            UpperBound
-        );
+         _renderMandelbrotSet(CurrentState);
     }
 
 
     p.mouseReleased = function(){
-        p.redraw();
+        setTimeout(p.redraw.bind(this), 500);
     }
 
-    const _renderMandelbrotSet = function(
-        max_iters,
-        colorEsc,
-        upperBound)
-    {
+    const _renderMandelbrotSet = function(state){
         for (let i = 0; i < WIDTH; i++) {
             for (let j = 0; j < HEIGHT; j++) {
 
@@ -70,7 +72,8 @@ export default new window.p5(function(p){
 
                 let num_iters = 0;
 
-                while (Z.magnitude() < upperBound && num_iters < max_iters) {
+                while (Z.magnitude() < state.upperBound &&
+                       num_iters < state.maxIterations) {
                     // Mandelbrot's equation: Zn+1 = Zn^2 + C
                     Z = Z.multiply(Z).add(C);
                     num_iters++;
@@ -78,24 +81,27 @@ export default new window.p5(function(p){
 
                 let colorValue;
 
-                if (colorEsc && num_iters === max_iters) {
+                if (state.escapeColoring && num_iters === state.maxIterations) {
                     colorValue = 0;
                 }
                 
-                colorValue = _normalizeToRGBValue(num_iters, max_iters);
-
+                colorValue = _normalizeRGBValue(num_iters, state.maxIterations);
+                Red = state.red + colorValue;
+                Green = state.green + colorValue;
+                Blue = state.blue + colorValue;
+                
                 let pixel = (i + j * WIDTH) * 4;
-                p.pixels[pixel] = colorValue;
-                p.pixels[pixel+1] = colorValue;
-                p.pixels[pixel+2] = colorValue;
-                p.pixels[pixel+3] = 250;
+                p.pixels[pixel] = Red > 255 ? _normalizeRGBValue(state.red + colorValue) : Red;
+                p.pixels[pixel+1] = Green > 255 ? _normalizeRGBValue(state.green + colorValue) : Green;
+                p.pixels[pixel+2] = Blue > 255 ? _normalizeRGBValue(state.blue + colorValue): Blue;
+                p.pixels[pixel+3] = 250;//_normalizeRGBValue(state.alpha + colorValue);
             }
         }
         p.updatePixels();
     }
 
 
-    const _normalizeToRGBValue = (val, maxValue) => {
+    const _normalizeRGBValue = (val, maxValue=510) => {
         // TODO: memoize me
       // Scale @param val from a range of 0 to @param maxValue
       //   to a range of 0 to 255 (RGB values)
